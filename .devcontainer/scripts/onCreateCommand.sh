67c19f69b43aab77dc59589358a790a025b273fb
Santiago Fernandez Madero <safern@microsoft.com>
2021-10-28 18:52:42 -0700

    Add .runsettings file to be able to run and debug tests on vscode in codespace with prebuilt

--- .devcontainer/scripts/onCreateCommand.sh
+++ .devcontainer/scripts/onCreateCommand.sh
@@ -4,3 +4,6 @@ set -e
 
 # prebuild the repo, so it is ready for development
 ./build.sh libs+clr -rc Release
+
+# generate runsettings file to be able to debug tests
+./dotnet.sh msbuild src/libraries/src.proj /t:GenerateRunSettingsFile /p:GenerateVsCodeRunSettings=true /p:EnableRunSettingsSupport=true /bl:artifacts/obj/vscode/runsettings.binlog
